8b64f02cd8837f487cc11a072e45054b05ebc9b9
Ryan Hileman <lunixbochs@gmail.com>
2015-02-09 01:37:08 -0800

    add Pack/UnpackWithOrder methods

--- struc.go
+++ struc.go
@@ -12,9 +12,31 @@ func Pack(w io.Writer, data interface{}) error {
 	return fields.Pack(w, data)
 }
 
+func PackWithOrder(w io.Writer, data interface{}, order int) error {
+	if fields, err := ParseFields(data); err == nil {
+		if err := fields.SetByteOrder(order); err != nil {
+			return err
+		}
+		return fields.Pack(w, data)
+	} else {
+		return err
+	}
+}
+
 func Unpack(r io.Reader, data interface{}) error {
 	fields, err := ParseFields(data)
 	if err != nil {
+		return err
+	}
+	return fields.Unpack(r, data)
+}
+
+func UnpackWithOrder(r io.Reader, data interface{}, order int) error {
+	fields, err := ParseFields(data)
+	if err != nil {
+		return err
+	}
+	if err := fields.SetByteOrder(order); err != nil {
 		return err
 	}
 	return fields.Unpack(r, data)
